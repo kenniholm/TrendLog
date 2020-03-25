9a266d1a9f3549f68684779028a82c4338a0d68c
Kenni Holm <kens123@hotmail.dk>
2020-03-25 18:19:57 +0100

    Remove unused import

--- app/src/main/java/com/example/trendlog/ui/RegisterViewModelFactory.kt
+++ app/src/main/java/com/example/trendlog/ui/RegisterViewModelFactory.kt
@@ -3,7 +3,6 @@ package com.example.trendlog.ui
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.trendlog.data.LoginRepository
 import java.lang.IllegalArgumentException
 
 class RegisterViewModelFactory(
